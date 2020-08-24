9e72153ea2ca440c53d82f5fdeff8e7981d42e4a
Larisa Steig <67192337+larisaSteig@users.noreply.github.com>
2020-08-23 21:57:22 -0600

    up to UPDATE routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose  = require('mongoose')
 const blog = require('./models/blog')
 const dotenv = require('dotenv').config()
-
+const methodOverride = require('method-override');
 const app        = express()
 const Blog = require("./models/blog")
 
@@ -21,12 +21,13 @@ db.once('open', function() {
 app.set("view engine", 'ejs')
 app.use (express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(methodOverride('_method'));
 //  RESTFUL ROUTES
 app.get("/", function(req, res){
   res.redirect("/blogs")
 })
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
   Blog.find({}, function(err, blogs){
     if (err){
@@ -37,20 +38,57 @@ app.get("/blogs", function(req, res){
   })
 })
 
+// NEW ROUTE
+app.get("/blogs/new", function(req,res){
+  res.render("new")
+})
 
+// CREATE ROUTE
 
+app.post("/blogs", function(req,res){
+  // create a blog
+  Blog.create (req.body.blog, function(err, newBlog){
+    if(err){
+      res.render("new")
+    } else {
+      res.redirect("/blogs")
+    }
+  })
+})
 
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+  Blog.findById(req.params.id, function(err, blog){
+    if(err){
+      console.log(err)
+      res.redirect("/blogs")
+    } else {
+      res.render("show", {blog: blog})
+    }
+  })
+})
 
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+  Blog.findById(req.params.id, function(err, blog){
+    if(err){
+      res.redirect("/blogs")
+    } else {
+      res.render("edit", {blog:blog})
+    }
+  })
+})
 
-
-
-
-
-
-
-
-
-
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+  Blog.findByIdAndUpdate(req.params.id,  req.body.blog,  function(err, blog){
+    if (err){
+      res.redirect("/blogs")
+    } else {
+      res.redirect ("/blogs/" + req.params.id)
+    }
+  })
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, function(){
